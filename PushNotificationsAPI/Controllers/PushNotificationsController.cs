d197a997f7286e8e00821018cd97f076e0ff5dc4
Felipe-Falcao <felipe.falcao@dcomp.ufs.br>
2022-03-03 13:02:25 -0300

    feat: api

---
+++ PushNotificationsAPI/Controllers/PushNotificationsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PushNotificationsAPI.Services;
+using System;
+
+namespace FirebasePushNotifications.Controllers
+{
+    public class PushNotificationsController : ControllerBase
+    {
+        [HttpPost]
+        [Route("/send-message")]
+        public String SendMessage([FromQuery] String topic, [FromQuery] String title, [FromQuery] String message)
+        {
+            //var topic = "all";
+            //var title = "Teste";
+            //var message = "Teste";
+
+            var firebase = new PushNotifications();
+            var response = firebase.SendMessage(topic, title, message);
+            return response;
+        }
+
+        [HttpPost]
+        [Route("/send-message/specific-device")]
+        public String SendToSpecificDeviceMessage([FromQuery] String token, [FromQuery] String title, [FromQuery] String message)
+        {
+            var firebase = new PushNotifications();
+            var response = firebase.SendToSpecificDeviceMessage(token, title, message);
+            return response;
+        }
+    }
+}
---
+++ PushNotificationsAPI/Services/PushNotifications.cs
@@ -0,0 +1,79 @@
+﻿using FirebaseAdmin;
+using FirebaseAdmin.Messaging;
+using Google.Apis.Auth.OAuth2;
+using System;
+using System.Collections.Generic;
+
+namespace PushNotificationsAPI.Services
+{
+    public class PushNotifications
+    {
+        public PushNotifications()
+        {
+            if(FirebaseApp.DefaultInstance == null)
+            {
+                FirebaseApp.Create(new AppOptions()
+                {
+                    Credential = GoogleCredential.FromFile("C:/Users/Felipe/source/repos/PushNotificationsAPI/PushNotificationsAPI/private_key.json"),
+                });
+            }
+        }
+
+        public String SendMessage(String topic, String title, String body)
+        {
+            var message = new Message()
+            {
+                Data = new Dictionary<string, string>()
+                {
+                    { "myData", "1337" },
+                },
+                Topic = topic,
+                Notification = new Notification()
+                {
+                    Title = title,
+                    Body = body
+                }
+            };
+
+            string response = FirebaseMessaging.DefaultInstance.SendAsync(message).Result;
+            return "Successfully sent message: " + response;
+        }
+
+        public String SendToSpecificDeviceMessage(String token, String title, String body)
+        {
+            var message = new Message()
+            {
+                Data = new Dictionary<string, string>()
+                {
+                    { "myData", "1337" },
+                },
+                Token = token,
+                Notification = new Notification()
+                {
+                    Title = title,
+                    Body = body
+                }
+            };
+
+            string response = FirebaseMessaging.DefaultInstance.SendAsync(message).Result;
+            return "Successfully sent message: " + response;
+        }
+
+        public void SendMulticastMessage(String body)
+        {
+            var message = new MulticastMessage()
+            {
+                Data = new Dictionary<string, string>()
+                {
+                    { "myData", "1337" },
+                },
+                Tokens = {},
+                Notification = new Notification()
+                {
+                    Title = "Title Test",
+                    Body = body
+                }
+            };
+        }
+    }
+}
